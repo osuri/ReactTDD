5b8d63474f9a54a69f5851389de9b6480002070b
Lokanadham Osuri <losuri@GLIC.COM>
2020-03-17 07:32:57 +0530

    added enzyme config in setup test file

--- wordgame/src/congrats.test.js
+++ wordgame/src/congrats.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import{ shallow } from 'enzyme';
+
 
 import Congrats from './congrats';
 import { findByTestAttr, checkProps } from '../test/testUtils';
 
 
-Enzyme.configure({adapter: new EnzymeAdapter()});
+
 const defaultProps = {success:false};
 const setup = (props={})=>{
     const setupProps = {...defaultProps,...props}
